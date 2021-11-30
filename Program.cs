b955572e19990ee25e3af5b62dc874f2f8bb4749
Valnei Rego <valneinfo@gmail.com>
2021-11-30 20:58:30 -0300

    Acrecendotado msg de Bom dia

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ namespace Teste
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom dia !!!!!");
+
         }
     }
 }
